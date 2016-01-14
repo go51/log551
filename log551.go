f6b5d03781631ba1ea4eb4bd4bfd6bfb6c900fa6
pubappbiz <ef+repository20160108@pubapp.biz>
2016-01-14 15:34:48 +0900

    refs #5 指定したファイルにログ情報を出力するメソッドを実装する

--- log551.go
+++ log551.go
@@ -1,4 +1,35 @@
 package log551
+import (
+	"os"
+	"fmt"
+	"time"
+)
+
+type logLevel int
+
+const (
+	DEBUG logLevel = iota
+	INFORMATION
+	WARNING
+	ERROR
+	CRITICAL
+)
+
+func (ll logLevel) String() string {
+	switch ll {
+	case DEBUG:
+		return "DEBUG"
+	case INFORMATION:
+		return "INFORMATION"
+	case WARNING:
+		return "WARNING"
+	case ERROR:
+		return "ERROR"
+	case CRITICAL:
+		return "CRITICAL"
+	}
+	return "NONE"
+}
 
 type Config struct {
 	Debug       bool `json:"debug"`
@@ -9,8 +40,130 @@ type Config struct {
 	Path        string `json:"path"`
 }
 
-type log551 struct {}
+type log551 struct {
+	config *Config
+	file   *os.File
+}
 
-func New() *log551 {
-	return &log551{}
+func New(config *Config) *log551 {
+	return &log551{
+		config:config,
+	}
+}
+
+func (l *log551) Open() {
+	if l.file != nil {
+		return
+	}
+
+	file, err := os.OpenFile(l.config.Path, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	l.file = file
+}
+
+func (l *log551) Close() {
+	err := l.file.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	l.file = nil
+}
+
+func (l *log551) Debug(a interface{}) {
+	l.output(DEBUG, "%v", a)
+}
+
+func (l *log551) Debugf(format string, a ...interface{}) {
+	l.output(DEBUG, format, a...)
+}
+
+func (l *log551) Debugln(a interface{}) {
+	l.output(DEBUG, "%v", a)
+}
+
+func (l *log551) Information(a interface{}) {
+	l.output(INFORMATION, "%v", a)
+}
+
+func (l *log551) Informationf(format string, a ...interface{}) {
+	l.output(INFORMATION, format, a...)
+}
+
+func (l *log551) Informationln(a interface{}) {
+	l.output(INFORMATION, "%v", a)
+}
+
+func (l *log551) Warning(a interface{}) {
+	l.output(WARNING, "%v", a)
+}
+
+func (l *log551) Warningf(format string, a ...interface{}) {
+	l.output(WARNING, format, a...)
+}
+
+func (l *log551) Warningln(a interface{}) {
+	l.output(WARNING, "%v", a)
+}
+
+func (l *log551) Error(a interface{}) {
+	l.output(ERROR, "%v", a)
+}
+
+func (l *log551) Errorf(format string, a ...interface{}) {
+	l.output(ERROR, format, a...)
+}
+
+func (l *log551) Errorln(a interface{}) {
+	l.output(ERROR, "%v", a)
+}
+
+func (l *log551) Critical(a interface{}) {
+	l.output(CRITICAL, "%v", a)
+}
+
+func (l *log551) Criticalf(format string, a ...interface{}) {
+	l.output(CRITICAL, format, a...)
+}
+
+func (l *log551) Criticalln(a interface{}) {
+	l.output(CRITICAL, "%v", a)
+}
+
+func (l *log551) output(level logLevel, format string, a ...interface{}) {
+	if ! l.isOutput(level) {
+		return
+	}
+
+	param := []interface{}{
+		time.Now().Format("2006/01/02 15:04:05.00000"),
+		level.String(),
+	}
+	param = append(param, a...)
+
+	fmt.Fprintf(l.file, "%s [%s] " + format + "\n", param...)
+
+}
+
+func (l *log551) isOutput(level logLevel) bool {
+	if !l.config.Debug && level == DEBUG {
+		return false
+	}
+	if !l.config.Information && level == INFORMATION {
+		return false
+	}
+	if !l.config.Warning && level == WARNING {
+		return false
+	}
+	if !l.config.Error && level == ERROR {
+		return false
+	}
+	if !l.config.Critical && level == CRITICAL {
+		return false
+	}
+
+	return true
 }
--- log551_test.go
+++ log551_test.go
@@ -5,8 +5,9 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	log1 := log551.New()
-	log2 := log551.New()
+
+	log1 := log551.New(nil)
+	log2 := log551.New(nil)
 
 	if log1 == nil {
 		t.Error("インスタンス生成に失敗しました。")
@@ -27,6 +28,196 @@ func TestNew(t *testing.T) {
 func BenchmarkNew(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = log551.New()
+		_ = log551.New(nil)
 	}
+}
+
+func TestDebug(t *testing.T) {
+	conf := log551.Config{
+		Debug:true,
+		Information:false,
+		Warning:false,
+		Error:false,
+		Critical:false,
+		Path:"/var/log/gorai/test.log",
+	}
+
+	l := log551.New(&conf)
+	l.Open()
+	defer l.Close()
+
+	l.Debug("test string")
+	l.Debugf("%s %s", "test", "string")
+	l.Debugln("test string")
+	l.Information("test string")
+	l.Informationf("%s %s", "test", "string")
+	l.Informationln("test string")
+	l.Warning("test string")
+	l.Warningf("%s %s", "test", "string")
+	l.Warningln("test string")
+	l.Error("test string")
+	l.Errorf("%s %s", "test", "string")
+	l.Errorln("test string")
+	l.Critical("test string")
+	l.Criticalf("%s %s", "test", "string")
+	l.Criticalln("test string")
+}
+
+func TestInformation(t *testing.T) {
+	conf := log551.Config{
+		Debug:false,
+		Information:true,
+		Warning:false,
+		Error:false,
+		Critical:false,
+		Path:"/var/log/gorai/test.log",
+	}
+
+	l := log551.New(&conf)
+	l.Open()
+	defer l.Close()
+
+	l.Debug("test string")
+	l.Debugf("%s %s", "test", "string")
+	l.Debugln("test string")
+	l.Information("test string")
+	l.Informationf("%s %s", "test", "string")
+	l.Informationln("test string")
+	l.Warning("test string")
+	l.Warningf("%s %s", "test", "string")
+	l.Warningln("test string")
+	l.Error("test string")
+	l.Errorf("%s %s", "test", "string")
+	l.Errorln("test string")
+	l.Critical("test string")
+	l.Criticalf("%s %s", "test", "string")
+	l.Criticalln("test string")
+}
+
+func TestWarning(t *testing.T) {
+	conf := log551.Config{
+		Debug:false,
+		Information:false,
+		Warning:true,
+		Error:false,
+		Critical:false,
+		Path:"/var/log/gorai/test.log",
+	}
+
+	l := log551.New(&conf)
+	l.Open()
+	defer l.Close()
+
+	l.Debug("test string")
+	l.Debugf("%s %s", "test", "string")
+	l.Debugln("test string")
+	l.Information("test string")
+	l.Informationf("%s %s", "test", "string")
+	l.Informationln("test string")
+	l.Warning("test string")
+	l.Warningf("%s %s", "test", "string")
+	l.Warningln("test string")
+	l.Error("test string")
+	l.Errorf("%s %s", "test", "string")
+	l.Errorln("test string")
+	l.Critical("test string")
+	l.Criticalf("%s %s", "test", "string")
+	l.Criticalln("test string")
+}
+
+func TestError(t *testing.T) {
+	conf := log551.Config{
+		Debug:false,
+		Information:false,
+		Warning:false,
+		Error:true,
+		Critical:false,
+		Path:"/var/log/gorai/test.log",
+	}
+
+	l := log551.New(&conf)
+	l.Open()
+	defer l.Close()
+
+	l.Debug("test string")
+	l.Debugf("%s %s", "test", "string")
+	l.Debugln("test string")
+	l.Information("test string")
+	l.Informationf("%s %s", "test", "string")
+	l.Informationln("test string")
+	l.Warning("test string")
+	l.Warningf("%s %s", "test", "string")
+	l.Warningln("test string")
+	l.Error("test string")
+	l.Errorf("%s %s", "test", "string")
+	l.Errorln("test string")
+	l.Critical("test string")
+	l.Criticalf("%s %s", "test", "string")
+	l.Criticalln("test string")
+}
+
+func TestCritical(t *testing.T) {
+	conf := log551.Config{
+		Debug:false,
+		Information:false,
+		Warning:false,
+		Error:false,
+		Critical:true,
+		Path:"/var/log/gorai/test.log",
+	}
+
+	l := log551.New(&conf)
+	l.Open()
+	defer l.Close()
+
+	l.Debug("test string")
+	l.Debugf("%s %s", "test", "string")
+	l.Debugln("test string")
+	l.Information("test string")
+	l.Informationf("%s %s", "test", "string")
+	l.Informationln("test string")
+	l.Warning("test string")
+	l.Warningf("%s %s", "test", "string")
+	l.Warningln("test string")
+	l.Error("test string")
+	l.Errorf("%s %s", "test", "string")
+	l.Errorln("test string")
+	l.Critical("test string")
+	l.Criticalf("%s %s", "test", "string")
+	l.Criticalln("test string")
+}
+
+func BenchmarkDebug(b *testing.B) {
+	conf := log551.Config{
+		Debug:true,
+		Information:true,
+		Warning:true,
+		Error:true,
+		Critical:true,
+		Path:"/var/log/gorai/test.log",
+	}
+
+	l := log551.New(&conf)
+	l.Open()
+	defer l.Close()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l.Debug("test string")
+		l.Debugf("%s %s", "test", "string")
+		l.Debugln("test string")
+		l.Information("test string")
+		l.Informationf("%s %s", "test", "string")
+		l.Informationln("test string")
+		l.Warning("test string")
+		l.Warningf("%s %s", "test", "string")
+		l.Warningln("test string")
+		l.Error("test string")
+		l.Errorf("%s %s", "test", "string")
+		l.Errorln("test string")
+		l.Critical("test string")
+		l.Criticalf("%s %s", "test", "string")
+		l.Criticalln("test string")
+	}
+
 }
