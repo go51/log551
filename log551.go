e8376f39c10d28f448405fcab10f09c2cf0cf77e
pubappbiz <ef+repository20160108@pubapp.biz>
2016-01-14 09:28:34 +0900

    refs #3 設定用の構造体を追加する

--- log551.go
+++ log551.go
@@ -1,5 +1,14 @@
 package log551
 
+type Config struct {
+	Debug       bool `json:"debug"`
+	Information bool `json:"information"`
+	Warning     bool `json:"warning"`
+	Error       bool `json:"error"`
+	Critical    bool `json:"critical"`
+	Path        string `json:"path"`
+}
+
 type log551 struct {}
 
 func New() *log551 {
